893c29c00f2ed9330d7f7f20e3224e527e0754ef
mrs-y-arai <sidvivid9672@gmail.com>
2025-05-17 16:50:01 +0900

    環境変数のキー名を修正: OPENAI_API_KEYからOPEN_AI_KEYに変更

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPEN_AI_KEY: process.env.OPEN_AI_KEY,
     API_BASE_URL: process.env.API_BASE_URL,
   },
 };
