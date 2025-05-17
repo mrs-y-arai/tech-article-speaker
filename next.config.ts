16672c253d502c555a24ff84c19a7076e5d3590b
mrs-y-arai <sidvivid9672@gmail.com>
2025-05-17 16:28:31 +0900

    batch api / env設定

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
   env: {
     SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    API_BASE_URL: process.env.API_BASE_URL,
   },
 };
 
